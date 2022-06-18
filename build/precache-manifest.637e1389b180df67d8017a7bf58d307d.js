2f54dc9742d2828f58c726aefabfaa364cb9eb23
DaianaServin <daiana.servin@hotmail.com>
2022-06-18 16:11:26 -0300

    Parcial de Frontend 3

---
+++ build/precache-manifest.637e1389b180df67d8017a7bf58d307d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23483e44c4528b07eee7",
+    "url": "/Frontend-III/entregable-frontend-3-junio22/static/css/main.108f2025.chunk.css"
+  },
+  {
+    "revision": "23483e44c4528b07eee7",
+    "url": "/Frontend-III/entregable-frontend-3-junio22/static/js/main.23483e44.chunk.js"
+  },
+  {
+    "revision": "ff2aeb385757ea353d47",
+    "url": "/Frontend-III/entregable-frontend-3-junio22/static/js/1.ff2aeb38.chunk.js"
+  },
+  {
+    "revision": "e2f7f596c13396a619cc",
+    "url": "/Frontend-III/entregable-frontend-3-junio22/static/js/runtime~main.e2f7f596.js"
+  },
+  {
+    "revision": "9ded6871f7cdadc50553ce9a447f15d4",
+    "url": "/Frontend-III/entregable-frontend-3-junio22/index.html"
+  }
+];
