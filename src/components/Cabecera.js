617b6d5b901c57cff764336e6ae584f9d99b20eb
DaianaServin <daiana.servin@hotmail.com>
2022-06-18 15:58:20 -0300

    Parcial de Frontend 3

--- src/components/Cabecera.js
+++ src/components/Cabecera.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 // El componente Cabera no tiene componentes hijos.
 // ESTADO: Cabecera no tiene estado.
@@ -8,11 +8,14 @@ import React from 'react'
 //    h1
 //    p > span     (el span mostrará la cantidad recibida por props)
 
-export default function Cabecera() {
+export default function Cabecera({contadorInicial}) {
   
   return (
     <header>
-        {/* maquetar Cabecera aquí */}
+        
+        <h1>Carrito de compras</h1>
+        <p>Cantidad de productos : <contador>{contadorInicial}</contador> </p>
     </header>
+
   )
 }
