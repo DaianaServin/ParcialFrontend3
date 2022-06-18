ad8e707178a0a4a51868cd8412ddd2503cc46f0e
DaianaServin <daiana.servin@hotmail.com>
2022-06-18 17:40:39 -0300

    Parcial de frontend 3

---
+++ build/precache-manifest.7c52c6920063d12f66a419784484e7f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23483e44c4528b07eee7",
+    "url": "/ParcialFrontend3/static/css/main.108f2025.chunk.css"
+  },
+  {
+    "revision": "23483e44c4528b07eee7",
+    "url": "/ParcialFrontend3/static/js/main.23483e44.chunk.js"
+  },
+  {
+    "revision": "ff2aeb385757ea353d47",
+    "url": "/ParcialFrontend3/static/js/1.ff2aeb38.chunk.js"
+  },
+  {
+    "revision": "1e4e9f1796b60533e882",
+    "url": "/ParcialFrontend3/static/js/runtime~main.1e4e9f17.js"
+  },
+  {
+    "revision": "15e9593bfa7500c71cc2f32a149a2fe4",
+    "url": "/ParcialFrontend3/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Frontend-III/entregable-frontend-3-junio22/precache-manifest.637e1389b180df67d8017a7bf58d307d.js"
+  "/ParcialFrontend3/precache-manifest.7c52c6920063d12f66a419784484e7f6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Frontend-III/entregable-frontend-3-junio22/index.html", {
+workbox.routing.registerNavigationRoute("/ParcialFrontend3/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
